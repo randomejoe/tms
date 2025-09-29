1482b49d0189af69e1a2e9fcc04a9904d5a21c54
Joe Davtian <joedavtian@gmail.com>
2025-09-29 11:09:46 +0200

    Ticket class added

---
+++ src/main/java/joe/davtian/tms/Ticket.java
@@ -0,0 +1,101 @@
+package joe.davtian.tms;
+import java.util.Date;
+
+public class Ticket {
+    private String id;
+    private String status;
+    private Date dateOfSubmission;
+    private Date deadline;
+    private String priority;
+    private int employeeID;
+    private String type;
+    private String subject;
+    private String description;
+
+    public Ticket(String id, String status, Date dateOfSubmission, Date deadline, String priority, int employeeID, String type, String subject, String description) {
+        this.id = id;
+        this.status = status;
+        this.dateOfSubmission = dateOfSubmission;
+        this.deadline = deadline;
+        this.priority = priority;
+        this.priority = priority;
+        this.employeeID = employeeID;
+        this.type = type;
+        this.subject = subject;
+        this.description = description;
+    }
+
+    public Ticket() {}
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public Date getDateOfSubmission() {
+        return dateOfSubmission;
+    }
+
+    public void setDateOfSubmission(Date dateOfSubmission) {
+        this.dateOfSubmission = dateOfSubmission;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
